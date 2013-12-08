b58ebe3e241f26cf5091256a00ac64322306ec2e
boB Rudis <bob@rudis.net>
2013-12-08 15:19:38 -0500

    adfa

--- R/pantone.R
+++ R/pantone.R
@@ -1,38 +1,41 @@
-pantone.2000 <- cerulean <- "#9bb7d4"
-pantone.2001 <- fuchsia.rose <- "#c74375"
-pantone.2002 <- true.red <- "#bf1932"
-pantone.2003 <- aqua.sky <- "#7bc4c4"
-pantone.2004 <- tigerlily <- "#e2583e"
-pantone.2005 <- blue.turquoise <- "#53b0ae"
-pantone.2006 <- sand.dollar <- "#decdbe"
-pantone.2007 <- chili.pepper <- "#9b1b30"
-pantone.2008 <- blue.iris <- "#5a5b9f"
-pantone.2009 <- mimosa <- "#f0c05a"
-pantone.2010 <- turquoise <- "#45b5aa"
-pantone.2011 <- honeysuckle <- "#d94f70"
-pantone.2012 <- tangerine.tango <- "#dd4124"
-pantone.2013 <- emerald <- "#009473"
-pantone.2014 <- radiant.orchid <- "#b163a3"
+#' @docType package
+#' @export pantone.coty.pal display.pantone.coty.pal
 
-pantone.coty.pal  <- c(pantone.2000, pantone.2001, pantone.2002, pantone.2003, pantone.2004,
-                       pantone.2005, pantone.2006, pantone.2007, pantone.2008, pantone.2009,
-                       pantone.2010, pantone.2011, pantone.2012, pantone.2013, pantone.2014)
+cerulean <- "#9bb7d4"
+fuchsia.rose <- "#c74375"
+true.red <- "#bf1932"
+aqua.sky <- "#7bc4c4"
+tigerlily <- "#e2583e"
+blue.turquoise <- "#53b0ae"
+sand.dollar <- "#decdbe"
+chili.pepper <- "#9b1b30"
+blue.iris <- "#5a5b9f"
+mimosa <- "#f0c05a"
+turquoise <- "#45b5aa"
+honeysuckle <- "#d94f70"
+tangerine.tango <- "#dd4124"
+emerald <- "#009473"
+radiant.orchid <- "#b163a3"
 
-pantone.coy.names <- c("Cerulean", "Fuchsia Rose", "True Red", "Aqua Sky", 
-                       "Tigerlily", "Blue Turquoise", "Sand Dollar",
-                       "Chili Pepper", "Blue Iris", "Mimosa", "Turquoise", 
-                       "Honeysuckle", "Tangerine Tango", "Emerald", 
-                       "Radiant Orchid")
+pantone.coty  <- c(cerulean, fuchsia.rose, true.red, aqua.sky, 
+                   tigerlily, blue.turquoise, sand.dollar, chili.pepper, 
+                   blue.iris, mimosa, turquoise, honeysuckle,
+                   tangerine.tango, emerald, radiant.orchid)
 
-names(pantone.coty.pal) <- pantone.coy.names
+pantone.coty.names <- c("Cerulean", "Fuchsia Rose", "True Red", "Aqua Sky", 
+                        "Tigerlily", "Blue Turquoise", "Sand Dollar",
+                        "Chili Pepper", "Blue Iris", "Mimosa", "Turquoise", 
+                        "Honeysuckle", "Tangerine Tango", "Emerald", 
+                        "Radiant Orchid")
+
+names(pantone.coty) <- pantone.coty.names
+
+pantone.coty.pal <- as.character(pantone.coty)
 
 display.pantone.coty.pal <- function (n=15) {
 
-  if (n < 3) {
-    warning("minimal value for n is 3, displaying requested palette with 3 different levels\n")
-    return(display.pantone.coty.pal(3, name))
-  }
   image(1:n, 1, as.matrix(1:n), col = pantone.coty.pal[1:n], 
-        xlab = paste(names(pantone.coty.pal)[1:n], collapse = "|"), ylab = "", xaxt = "n", yaxt = "n", 
-        bty = "n")
+        xlab = paste(pantone.coty[1:n], collapse = ", "), ylab = "", xaxt = "n", yaxt = "n", 
+        bty = "n", cex.lab=0.80)
+  
 }
